63f38788fcc224ff2c43e0558c7aab27d48e0a45
Luiz Gabriel <luiz.sena@ufam.edu.br>
2024-03-14 16:49:46 -0400

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]]
-
-const newItems = _.flattenDeep(items)
-
-console.log(newItems)
+console.log("first")
+setTimeout(() => {
+    console.log("second")
+},0)
+console.log("third")
+console.log("fourth")
+setTimeout(() => {
+    console.log("Fifth")
+})
